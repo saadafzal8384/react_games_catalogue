273ccb2559e079046fb3ae547b151ac43ec5e0d0
Saad <mu.saadafzal@gmail.com>
2022-07-19 21:35:25 +0100

    added action buttons in game details view

--- src/components/GameInformation/GameInformation.jsx
+++ src/components/GameInformation/GameInformation.jsx
@@ -2,6 +2,12 @@ import { Box, Chip, Grid, Rating, Stack, Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { gamesOptions, fetchData } from "../../utils/fetchData";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import RedditIcon from "@mui/icons-material/Reddit";
+import GradeIcon from "@mui/icons-material/Grade";
+import LanguageIcon from "@mui/icons-material/Language";
 
 const GameInformation = () => {
   const { id } = useParams();
@@ -18,7 +24,7 @@ const GameInformation = () => {
       console.log(gamesData);
     };
     fetchGameData();
-  }, []);
+  }, [id]);
 
   return (
     <Grid container>
@@ -44,32 +50,51 @@ const GameInformation = () => {
         <Typography variant="body1" gutterBottom align="justify" m={2}>
           {game?.description_raw}
         </Typography>
-        <Typography variant="h6" gutterBottom align="justify" m={2}>
-          Developers:{" "}
-          {game?.developers.slice(0, 1).map((developer, id) => (
-            <Chip
-              sx={{ background: "orange", color: "#000", fontWeight: "bold" }}
-              key={id}
-              label={developer.name}
-            />
-          ))}
+        <Stack direction="row" spacing={2} sx={{ marginLeft: "1rem" }}>
+          <Button
+            variant="contained"
+            color="success"
+            endIcon={<AccessTimeFilledIcon />}
+          >
+            {game?.playtime} Hours
+          </Button>
+          <Button
+            variant="contained"
+            target="_blank"
+            rel="noopener noreferr"
+            color="info"
+            href={game?.reddit_url}
+            endIcon={<GradeIcon />}
+          >
+            {`${game?.rating} / ${game?.rating_top}`}
+          </Button>
+          <Button
+            variant="contained"
+            target="_blank"
+            rel="noopener noreferr"
+            color="error"
+            href={game?.reddit_url}
+            endIcon={<RedditIcon />}
+          >
+            {game?.reddit_name}
+          </Button>
+          <Button
+            variant="contained"
+            target="_blank"
+            rel="noopener noreferr"
+            color="error"
+            href={game?.website}
+            endIcon={<LanguageIcon />}
+          >
+            Website
+          </Button>
+        </Stack>
+        {/* <Typography variant="subtitle1" gutterBottom align="justify" m={2}>
+          &nbsp;Playtime :&nbsp; {game?.playtime} Hours
         </Typography>
-        <Typography
-          variant="h6"
-          gutterBottom
-          align="justify"
-          sx={{ marginTop: "10px" }}
-          m={2}
-        >
-          Genres :{" "}
-          {game?.genres.slice(0, 3).map((genre, id) => (
-            <Chip
-              sx={{ background: "green", color: "#FFF", marginRight: "10px" }}
-              key={id}
-              label={genre.name}
-            />
-          ))}
-        </Typography>
+        <Typography variant="subtitle1" gutterBottom align="justify" m={2}>
+          &nbsp;Playtime :&nbsp; {game?.playtime} Hours
+        </Typography> */}
       </Grid>
     </Grid>
   );
